b6bed22b0c100a1c072896a3ce0d9f1496c83303
Shilin Victor <chrono.monochrome@gmail.com>
2016-02-20 23:11:14 +0700

    FORCE_ARM: don't use on HOST && CLANG
    Change-Id: I748db839359fc57131e2a20fc66b6f8fe9b7c09d

--- core/binary.mk
+++ core/binary.mk
@@ -597,6 +597,9 @@ endif
 
 ifeq ($(FORCE_ARM),true)
 
+ifeq ($(LOCAL_IS_HOST_MODULE),)
+ifneq ($(LOCAL_CLANG),true)
+
   ifneq (1,$(words $(filter $(LOCAL_FORCE_ARM_EXCLUSION_LIST),$(LOCAL_MODULE))))
     ifdef LOCAL_CFLAGS
       LOCAL_CONLYFLAGS += -marm
@@ -609,6 +612,9 @@ ifeq ($(FORCE_ARM),true)
       LOCAL_CPPFLAGS := -marm
     endif
   endif
+
+endif
+endif
 
 endif
 
