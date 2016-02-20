37b249064a543e627942b169dd241ddf49c33c35
Shilin Victor <chrono.monochrome@gmail.com>
2016-02-21 06:24:22 +0700

    Fix compilation of some modules
    Change-Id: I72d3dd91e47b7f4e3212e76b6e0ae5a7cca8dbbf

--- core/optipop.mk
+++ core/optipop.mk
@@ -304,6 +304,15 @@ LOCAL_DISABLE_PIPE := \
 # STRICT_ALIASING   #
 #####################
 LOCAL_DISABLE_STRICT := \
+    libutils \
+    libncurses \
+    libhwui \
+    backtrace_test \
+    ltrace \
+    tcpdump \
+    memtrack \
+    idmap \
+    libstagefright \
     third_party_libyuv_libyuv_gyp \
     third_party_WebKit_Source_wtf_wtf_gyp \
     ipc_ipc_gyp \
@@ -728,6 +737,7 @@ WILHELM_LIBRARIES := \
 #######
 
 LOCAL_DISABLE_LTO := \
+	libutils \
 	audioflinger \
 	libxz \
 	libscheduling_policy \
@@ -1145,6 +1155,15 @@ LOCAL_DISABLE_LTO := \
 	libhardware \
 
 LOCAL_FORCE_ARM_EXCLUSION_LIST := \
+    libncurses \
+    libutils \
+    libhwui \
+    backtrace_test \
+    ltrace \
+    tcpdump \
+    memtrack \
+    idmap \
+    libstagefright \
 	audioflinger \
 	skia_skia_library_gyp \
 	libasound \
