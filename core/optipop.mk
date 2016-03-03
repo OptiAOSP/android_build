505658fb3f35f82542984cfb82888aaec6057ca3
Shilin Victor <chrono.monochrome@gmail.com>
2016-03-04 00:42:20 +0700

    Add my email to optimization rules files
    Change-Id: Ib232b9170f88c95acddd52dafef201cdf6fb5f35

--- core/optipop.mk
+++ core/optipop.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2014-2015 OptiPop
+#           (C) 2016 Shilin Victor <chrono.monochrome@gmail.com>.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
