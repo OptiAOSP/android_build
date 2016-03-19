b448bec4e94de92a216890490f99027427c39525
Shilin Victor <chrono.monochrome@gmail.com>
2016-03-19 21:08:48 +0700

    Revert changes to armv7-a-neon.mk
    Change-Id: I24f27a269910857bc2cc4b7b6bf28c26683cbf34

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -107,7 +107,7 @@ $(combo_2nd_arch_prefix)TARGET_GLOBAL_CFLAGS += \
 			-fno-short-enums \
 			-no-canonical-prefixes \
 			-fno-canonical-system-headers \
-			-mcpu=cortex-a9 -mtune=cortex-a9 \
+			$(arch_variant_cflags) \
 			-include $(android_config_h) \
 			-I $(dir $(android_config_h))
 
